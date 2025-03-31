7089c6220e826254274eca3a27da2a70f006c7d2
MiguelMorello <guelmorello@gmail.com>
2025-03-31 19:41:41 -0300

    Arrumando o teste 1

--- Teste1/scraper/scraper.py
+++ Teste1/scraper/scraper.py
@@ -1,8 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
 import zipfile
 import os
 
+
+def download_pdf(url, download_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(download_path, 'wb') as f:
+            f.write(response.content)
+        print(f"Arquivo {download_path} baixado com sucesso!")
+    else:
+        print(f"Falha ao baixar o arquivo de {url}")
+
+
+def find_pdf_links(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        pdf_links = []
+        for a_tag in soup.find_all('a', href=True):
+            link = a_tag['href']
+            
+            if 'pdf' in link.lower() and ('Anexo I' in a_tag.get_text() or 'Anexo II' in a_tag.get_text()):
+                pdf_links.append(link if link.startswith('http') else url + link)
+        return pdf_links
+    else:
+        print(f"Falha ao acessar o site {url}")
+        return []
+
+
 def zip_pdfs(pdf_files, output_zip):
-    
     with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for pdf in pdf_files:
             if os.path.exists(pdf):
@@ -12,11 +41,26 @@ def zip_pdfs(pdf_files, output_zip):
     print(f"Arquivos compactados com sucesso em {output_zip}")
 
 
-pdf_files = ['data/Anexo_I.pdf', 'data/Anexo_II.pdf']
+url = 'https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos'
 
 
-output_zip = 'Anexos_test.zip'
+pdf_links = find_pdf_links(url)
 
 
+download_dir = 'data/'
+
+
+if not os.path.exists(download_dir):
+    os.makedirs(download_dir)
+
+
+pdf_files = []
+for link in pdf_links:
+    filename = link.split('/')[-1]
+    file_path = os.path.join(download_dir, filename)
+    download_pdf(link, file_path)
+    pdf_files.append(file_path)
+
+
+output_zip = 'Anexos.zip'
 zip_pdfs(pdf_files, output_zip)
-
--- Teste1/scraper/testes/test_scraper.py
+++ Teste1/scraper/testes/test_scraper.py
@@ -1,39 +1,71 @@
 import unittest
+from unittest.mock import patch, MagicMock
 import os
-import zipfile
-from Teste_Nivelamento.Teste1.scraper.scraper import zip_pdfs  
+from Teste_Nivelamento.Teste1.scraper.scraper import download_pdf, find_pdf_links, zip_pdfs
 
 class TestScraper(unittest.TestCase):
 
-    def setUp(self):
-       
-        self.pdf_files = ['Teste_Nivelamento\data\Anexo_I.pdf', 'Teste_Nivelamento\data\Anexo_II.pdf']
-        self.output_zip = 'Anexos_test.zip'
+    @patch('requests.get')
+    def test_download_pdf(self, mock_get):
+        
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.content = 'Teste de conteúdo do PDF'
+        mock_get.return_value = mock_response
         
         
-        os.makedirs(os.path.dirname(self.pdf_files[0]), exist_ok=True)
-        for pdf in self.pdf_files:
-            if not os.path.exists(pdf):  
-                with open(pdf, 'w') as f:
-                    f.write("Conteúdo do arquivo PDF")
-        
-    def test_zip_pdfs(self):
-        
-        for pdf in self.pdf_files:
-            self.assertTrue(os.path.exists(pdf), f"O arquivo {pdf} não foi encontrado.")
-        
-        
-        zip_pdfs(self.pdf_files, self.output_zip)
+        download_pdf('https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf', 'data/teste.pdf')
 
-      
-        self.assertTrue(os.path.exists(self.output_zip), f"O arquivo {self.output_zip} não foi criado.")
+        
+        mock_get.assert_called_once_with('https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf')
 
-       
-        if os.path.exists(self.output_zip):
-            with zipfile.ZipFile(self.output_zip, 'r') as zipf:
-                zip_contents = zipf.namelist()
-                for pdf in self.pdf_files:
-                    self.assertIn(os.path.basename(pdf), zip_contents, f"{os.path.basename(pdf)} não está no ZIP.")
+        
+        self.assertTrue(os.path.exists('data/teste.pdf'))
+        
+        
+        os.remove('data/teste.pdf')
+
+    @patch('requests.get')
+    def test_find_pdf_links(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.text = '''
+        <html>
+            <body>
+                <a href="Anexo_I.pdf">Anexo I</a>
+                <a href="Anexo_II.pdf">Anexo II</a>
+            </body>
+        </html>
+        '''
+        mock_get.return_value = mock_response
+        
+        
+        pdf_links = find_pdf_links('https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos')
+
+        # Verifica se os links "Anexo I" e "Anexo II" foram encontrados
+        self.assertEqual(len(pdf_links), 2)
+        self.assertIn('Anexo_I.pdf', pdf_links)
+        self.assertIn('Anexo_II.pdf', pdf_links)
     
+    
+    def test_zip_pdfs(self):
+        test_files = ['data/teste1.pdf', 'data/teste2.pdf']
+        
+        os.makedirs('data', exist_ok=True)
+
+        for file in test_files:
+            with open(file, 'w') as f:
+                f.write('Conteúdo de teste')
+
+
+        zip_pdfs(test_files, 'data/teste.zip')
+
+        
+        self.assertTrue(os.path.exists('data/teste.zip'))
+
+        os.remove('data/teste1.pdf')
+        os.remove('data/teste2.pdf')
+        os.remove('data/teste.zip')
+
 if __name__ == '__main__':
     unittest.main()
